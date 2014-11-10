6d552c4e282da0affcbdb57112de1361c060e9bd
Matt Zumwalt <matt@curationexperts.com>
2014-11-10 10:52:59 +0100

    Adds OAI-PMH response; Provides a placeholder for rendering complete mxd metadata

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,6 +1,6 @@
 class CatalogController < ApplicationController
   include Worthwhile::CatalogController
-  include BlacklightOaiProvider::ControllerExtension
+  include ::BlacklightOaiProvider::ControllerExtension
 
   configure_blacklight do |config|
     config.oai = {
@@ -16,4 +16,25 @@ class CatalogController < ApplicationController
         }
     }
   end
+
+  # get search results from the solr index
+  # Overriding this method in order to enable rendering of OAI-PMH responses when .oaipmh is the requested format
+  def index
+    (@response, @document_list) = get_search_results
+
+    respond_to do |format|
+      format.html { }
+      format.rss  { render :layout => false }
+      format.atom { render :layout => false }
+      format.oaipmh { render :layout => false }     # <-- This is the only line that we actually want to override.
+      format.json do
+        render json: render_search_results_as_json
+      end
+
+      additional_response_formats(format)
+      document_export_formats(format)
+    end
+  end
+
+
 end
