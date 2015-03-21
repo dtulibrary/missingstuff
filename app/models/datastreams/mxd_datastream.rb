19ffaba1579b398e7ce623232c6593fcc22ec5f0
franck <franck@cvt.dk>
2015-03-21 16:25:43 +0100

    change format definition to match new form model

--- app/models/datastreams/mxd_datastream.rb
+++ app/models/datastreams/mxd_datastream.rb
@@ -7,23 +7,25 @@ class MxdDatastream < ActiveFedora::OmDatastream
   set_terminology do |t|
     t.root(:path=>"mxd")
     t.lang
-    t.review
-    t.level
     t.title(index_as: [:stored_searchable, :facetable])
-    t.subtitle
+    t.subtitle(index_as: [:stored_searchable, :facetable])
+
     t.description(index_as: [:stored_searchable, :facetable])
     t.keyword(index_as: [:stored_searchable, :facetable])
     t.person {
+      t.person_id_type
+      t.person_id(index_as: [:stored_searchable, :facetable])
       t.first_name(index_as: [:stored_searchable, :facetable])
       t.last_name(index_as: [:stored_searchable, :facetable])
       t.role
+      t.affiliation
     }
     t.organisation {
-      t.level1
-      t.level2
-      t.level3
-      t.level4
-      t.role
+      t.name
+    }
+    t.editor {
+      t.first_name(index_as: [:stored_searchable, :facetable])
+      t.last_name(index_as: [:stored_searchable, :facetable])
     }
     t.event {
         t.title
@@ -31,11 +33,34 @@ class MxdDatastream < ActiveFedora::OmDatastream
         t.end
         t.place
     }
+    t.details {
+        t.page_range
+        t.chapter
+        t.pages
+        t.doi
+        t.article_number
+    }
     t.book {
         t.book_title
+        t.book_subtitle
         t.publisher
+        t.city
+        t.year
+        t.volume
         t.edition
+        t.doi
         t.isbn
+        t.eisbn
+    }
+    t.series {
+        t.series_title
+        t.year
+        t.volume
+        t.number
+        t.country
+        t.issn
+        t.eissn
+        t.publisher
     }
     t.requester
     t.publication_date(index_as: [:stored_searchable, :facetable])
