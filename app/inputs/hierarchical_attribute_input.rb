a7ed82a96d70891520a65159b1bc47c647b123e4
franck <franck@cvt.dk>
2015-03-22 10:58:50 +0100

    Added support for templates

--- app/inputs/hierarchical_attribute_input.rb
+++ app/inputs/hierarchical_attribute_input.rb
@@ -8,26 +8,44 @@ class HierarchicalAttributeInput < SimpleForm::Inputs::CollectionInput
     input_html_options[:name] ||= "#{object_name}[#{attribute_name}][]"
     subfields = input_html_options[:subfields] ? input_html_options.delete(:subfields) : {}
     repeatable = input_html_options[:repeatable] ? input_html_options.delete(:repeatable) : ''
+    template = input_html_options[:template] ? input_html_options.delete(:template) : nil
+    record_type = input_html_options[:record_type] ? input_html_options.delete(:record_type) : nil
 
     populated_field_groups = ""
     collection.each_with_index do |entry,i|
       unless entry.to_s.strip.blank?
-        populated_field_groups << <<-HTML
-          <li class="field-wrapper hierarchical">
-            #{build_subfields(subfields, entry, i)}
-          </li>
-        HTML
+        if template
+          populated_field_groups << <<-HTML
+            <li class="field-wrapper hierarchical">
+              #{build_template(template, record_type, subfields, entry, i)}
+            </li>
+          HTML
+        else
+          populated_field_groups << <<-HTML
+            <li class="field-wrapper hierarchical">
+              #{build_subfields(subfields, entry, i)}
+            </li>
+          HTML
+        end
       end
     end
 
     if repeatable == 'false' && !populated_field_groups.blank?
       empty_field_group = ''
     else
-      empty_field_group = <<-HTML
-        <li class="field-wrapper hierarchical">
-          #{build_subfields(subfields, nil, nil)}
-        </li>
-      HTML
+      if template
+        empty_field_group = <<-HTML
+          <li class="field-wrapper hierarchical">
+            #{build_template(template, record_type, subfields, nil, nil)}
+          </li>
+        HTML
+      else
+        empty_field_group = <<-HTML
+          <li class="field-wrapper hierarchical">
+            #{build_subfields(subfields, nil, nil)}
+          </li>
+        HTML
+      end
     end
 
     markup = <<-HTML
@@ -46,24 +64,50 @@ class HierarchicalAttributeInput < SimpleForm::Inputs::CollectionInput
   def build_subfields(subfields, entry, index=nil)
     markup = ""
     subfields_for(object, attribute_name).each do |subfield_name|
-      if entry.nil? || entry.empty?
-        value = entry
-      else
-        value = entry.send(subfield_name).first
-      end
-      if ! subfields[subfield_name] || ! subfields[subfield_name][:skip]
+      if !subfields[subfield_name] || !subfields[subfield_name][:skip]
+        if entry.nil? || entry.empty?
+          value = entry
+        else
+          value = entry.send(subfield_name).first
+        end
         markup << build_form_group(subfields, value, subfield_name, index:index, label: subfield_label(attribute_name, subfield_name))
-       end
+      end
     end
     markup
+  end
+
+  def build_template(template, record_type, subfields, entry, index=nil)
+    values = {}
+    subfields_for(object, attribute_name).each do |subfield_name|
+      if !subfields[subfield_name] || !subfields[subfield_name][:skip]
+        if entry.nil? || entry.empty?
+          value = entry
+        else
+          value = entry.send(subfield_name).first
+        end
+        if subfields[subfield_name]
+          case subfields[subfield_name][:type]
+            when 'select'
+              values[subfield_name] = build_select(subfields[subfield_name][:options], value, subfield_name.to_s, index)
+            else
+              values[subfield_name] = build_text_field(value, subfield_name.to_s, index)
+          end
+        else
+          values[subfield_name] = build_text_field(value, subfield_name.to_s, index)
+        end
+      end
+    end
+#   render "curation_concern/base/#{template}", subfields: subfields, value: value, index: index
+#   ApplicationController.new.render_to_string :template => "curation_concern/base/_#{template}"
+    markup = ActionController::Base.new.render_to_string :template => "form_templates/#{template}", :locals => {values: values, record_type: record_type}
   end
 
   def build_form_group(subfields, value, attribute_name, options={})
     group_label = options.fetch(:label, false) ? options.fetch(:label) : attribute_name.to_s.humanize
     markup = <<-HTML
       <div class="form-group">
-        <label class="col-sm-2 control-label">#{group_label}</label>
-        <div class="col-sm-10">
+        <label class="col-sm-1 control-label">#{group_label}</label>
+        <div class="col-sm-11">
     HTML
     if subfields[attribute_name]
       case subfields[attribute_name][:type]
